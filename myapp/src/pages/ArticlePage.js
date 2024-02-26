5285327856bb347dc0f3a7f0e8639c5cabc4ef81
prameela <prameelarpujari>
2024-02-26 17:02:20 +0530

    Individual Article page for all articles is completed.

--- myapp/src/pages/ArticlePage.js
+++ myapp/src/pages/ArticlePage.js
@@ -1,9 +1,30 @@
-function ArticlePage(){
-    return(
-     <>
-     Article Page
-     </>
-    );
-}
+import { useParams} from "react-router-dom";
+import articles from "./articles-content";
+import NotFoundPage from "./NotFoundPage";
 
+function ArticlePage(){
+    const {articleId} = useParams();
+    const article = articles.find(article=>
+        article.name === articleId);
+        
+    if(article){
+        return(
+            <>
+            <h1>{article.title}</h1>
+
+            {article.content.map((paragraph,i) =>(
+                <div key={i}>
+                 {paragraph}
+                </div>
+                   
+             
+            ))}
+            </>
+        );
+    }
+    else{
+        return <NotFoundPage></NotFoundPage>
+       }
+    }
+    
 export default ArticlePage;
--- myapp/src/pages/articles-content.js
+++ myapp/src/pages/articles-content.js
@@ -1,24 +1,24 @@
 const articles=[
   {
-   name:"split-str-js",
+   name:"split-js",
    title:"Best way to learn split",
    content:["This is first articles first content", 
             "This is first articles second content",
-            "This is first articles third content"]
+            "This is first articles third content"],
   },
   {
     name:"map-js",
     title:"Awesome way to learn map",
     content:["This is second articles first content", 
              "This is second articles second content",
-             "This is second articles third content"]
+             "This is second articles third content"],
    },
    {
     name:"filter-js",
     title:"Best way to learn filter",
     content:["This is Third articles first content", 
              "This is Third articles second content",
-             "This is Third articles third content"]
+             "This is Third articles third content"],
    },
 ];
 
