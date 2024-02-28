b86ca0a04601967f7218761f372b8575e5f5f3bb
prameela <prameelarpujari>
2024-02-28 16:11:03 +0530

    Created back-end folder. Created server api. Created a dummy db articleInfo.

--- myapp-backend/src/server.js
+++ myapp-backend/src/server.js
@@ -1,1 +1,34 @@
-console.log("Hello");
+import express from "express";
+
+let articleInfo =[
+     {
+          name: "split-js",
+          likes: 0
+     },
+     {
+          name: "map-js",
+          likes: 0
+     },
+     {
+          name: "filter-js",
+          likes: 0
+     }
+   ];
+const app=express();
+app.use(express.json());
+
+app.put("/api/articles/:name/likes", (req, res)=>{
+      const {name} = req.params;
+      const article = articleInfo.find(article => article.name===name);
+      if(article){
+          article.likes +=1;
+          res.send(`The article ${article.name} has ${article.likes} likes!`);
+      }
+      else{
+          res.send(`article not found`);
+      }
+});
+
+app.listen(8000, ()=>{
+     console.log("listening on 8000");
+});
